df700aa7de1bd3b23fd6175ec72729cb04551332
repo <reporepo254@gmail.com>
2023-02-10 23:16:50 +0900

    first commit

---
+++ front/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>test</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {data} = await useFetch('https://jsonplaceholder.typicode.com/posts/');
+
+console.log(data.value)
+
+const test = await useFetch('https://jsonplaceholder.typicode.com/posts/');
+
+console.log(test)
+console.log(test.data.value)
+
+const go = await useFetch('http://localhost:8080/albums');
+console.log(go.data.value)
+
+</script>
